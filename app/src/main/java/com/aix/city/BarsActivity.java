c8ca6ac5c4cd1097757d971ef9e5833193349c3f
Henric <henric.breuer@gmail.com>
2015-10-23 20:10:17 +0200

    onClickListener for menue implemented
    Bars Activity added if bar is clicked on menue
       later on Bars activity will start with Tags dynamically

---
+++ app/src/main/java/com/aix/city/BarsActivity.java
@@ -0,0 +1,42 @@
+package com.aix.city;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.aix.city.view.PostAdapter;
+
+public class BarsActivity extends FragmentActivity implements ListingBarFragment.OnFragmentInteractionListener {
+
+    private ListView mainListView ;
+    private ListView menue_list;
+    ArrayAdapter<String> listAdapter;
+    String[] institutions = {"post", "bars", "restaurtants"};
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bars_activity);
+
+        String string = getIntent().getStringExtra("Bars");
+
+        Toast toast = Toast.makeText(this, string, Toast.LENGTH_LONG);
+        toast.show();
+
+
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
+    }
+}
--- app/src/main/java/com/aix/city/ListActivity.java
+++ app/src/main/java/com/aix/city/ListActivity.java
@@ -1,12 +1,15 @@
 package com.aix.city;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import com.aix.city.view.PostAdapter;
 
 public class ListActivity extends FragmentActivity implements ListingFragment.OnFragmentInteractionListener {
 
@@ -21,11 +24,44 @@ public class ListActivity extends FragmentActivity implements ListingFragment.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String string = getIntent().getStringExtra("Bars");
+
+        Toast toast = Toast.makeText(this, string, Toast.LENGTH_LONG);
+        toast.show();
+
+
         menue_list = (ListView) findViewById(R.id.list_menue);
         listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,institutions);
         menue_list.setAdapter(listAdapter);
 
 
+        menue_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent;
+
+
+                switch (position) {
+                    case 0:
+                        intent = new Intent(getApplicationContext(), ListActivity.class);
+                        break;
+                    case 1:
+                        intent = new Intent(getApplicationContext(), BarsActivity.class);
+                        break;
+                    case 2:
+                        intent = new Intent(getApplicationContext(), ListActivity.class);
+                        break;
+                    default:
+                        intent = new Intent(getApplicationContext(), ListActivity.class);
+                        break;
+                }
+                intent.putExtra("Bars","Bars");
+                startActivity(intent);
+
+            }
+        });
 
     }
 
