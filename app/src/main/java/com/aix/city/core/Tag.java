1f55322c47b8b1b4351ce9b07a8ab5b2ea7ad4eb
Henric <henric.breuer@gmail.com>
2015-10-23 17:51:34 +0200

    201510231751

--- app/src/main/java/com/aix/city/core/Tag.java
+++ app/src/main/java/com/aix/city/core/Tag.java
@@ -33,4 +33,5 @@ public class Tag extends Group {
         }
         return listing;
     }
+
 }
---
+++ app/src/main/java/com/aix/city/core/UserProfileFragment.java
@@ -0,0 +1,22 @@
+package com.aix.city.core;
+
+/**
+ * Created by Henric on 23.10.15.
+ */
+public class UserProfileFragment {
+
+    private User _user;
+
+    public void onCreate(User comUser){
+        _user = comUser;
+    }
+
+    public View onCreateView(){
+        return null;
+    }
+
+    public void onPause(){
+
+    }
+
+}
