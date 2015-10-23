7b68de95ef1fdb0d6297fbc7a8ef9f8439f0f68a
JustinPhung <justinbao@hotmail.de>
2015-10-23 18:56:13 +0200

    slide menu hinzugefügt

--- app/src/main/java/com/aix/city/ListActivity.java
+++ app/src/main/java/com/aix/city/ListActivity.java
@@ -3,6 +3,7 @@ package com.aix.city;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.aix.city.view.PostAdapter;
@@ -10,13 +11,21 @@ import com.aix.city.view.PostAdapter;
 public class ListActivity extends FragmentActivity implements ListingFragment.OnFragmentInteractionListener {
 
     private ListView mainListView ;
-    private PostAdapter listAdapter ;
+    private ListView menue_list;
+    ArrayAdapter<String> listAdapter;
+    String[] institutions = {"post", "bars", "restaurtants"};
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menue_list = (ListView) findViewById(R.id.list_menue);
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,institutions);
+        menue_list.setAdapter(listAdapter);
+
+
 
     }
 
